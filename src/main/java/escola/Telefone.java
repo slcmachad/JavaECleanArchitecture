284d250e37cec26253614542330144de02355249
Salomao <salomao.machado.75@gmail.com>
2023-07-17 16:00:11 -0300

    <build> Criando classe Telefone que servira como parte dos contatos.

---
+++ src/main/java/escola/Telefone.java
@@ -0,0 +1,13 @@
+package escola;
+
+public class Telefone {
+
+	private String ddd;
+	private String numero;
+	public Telefone(String ddd, String numero) {
+		ddd = ddd;
+		this.numero = numero;
+	}
+	
+	
+}
