e8cedfc86c5c19e0048fce717b422cd27864430a
Salomao <salomao.machado.75@gmail.com>
2023-07-17 15:49:33 -0300

    <Test> Criando testes, verificando validações de email.

---
+++ src/test/java/escola/EmailTest.java
@@ -0,0 +1,13 @@
+package escola;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmailTest {
+
+	@Test
+	void naoDeveriaCriarEmailComEnderecosInvalidos() {
+	}
+
+}
