d76090aada2c3bbbb65e629e0f787215cbbe7389
Salomao <salomao.machado.75@gmail.com>
2023-07-17 15:52:28 -0300

    <test> Criando teste para a parte de validações quando um email é invalido.

--- src/test/java/escola/EmailTest.java
+++ src/test/java/escola/EmailTest.java
@@ -8,6 +8,9 @@ class EmailTest {
 
 	@Test
 	void naoDeveriaCriarEmailComEnderecosInvalidos() {
+		assertThrows(IllegalArgumentException.class, () -> new Email(null));
+		assertThrows(IllegalArgumentException.class, () -> new Email(""));
+		assertThrows(IllegalArgumentException.class, () -> new Email("emailInvalido"));
 	}
 
 }
