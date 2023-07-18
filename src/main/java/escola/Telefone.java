2a01efcd00f482d8a413d5c3e27be97e14737f4b
Salomao <salomao.machado.75@gmail.com>
2023-07-18 20:32:00 -0300

    <build> Criando classe e construtor para o telefone.

--- src/main/java/escola/Telefone.java
+++ src/main/java/escola/Telefone.java
@@ -5,8 +5,19 @@ public class Telefone {
 	private String ddd;
 	private String numero;
 	public Telefone(String ddd, String numero) {
-		ddd = ddd;
-		this.numero = numero;
+	    if (ddd == null || numero == null) {
+	        throw new IllegalArgumentException("DDD e Numero sao obrigatorios!");
+	    }
+
+	    if (!ddd.matches("\\d{2}")) {
+	        throw new IllegalArgumentException("DDD invalido!");
+	    }
+
+	    if (!numero.matches("\\d{8}|\\d{9}")) {
+	        throw new IllegalArgumentException("Numero invalido!");
+	    }
+	    this.ddd = ddd;
+	    this.numero = numero;
 	}
 	
 	
