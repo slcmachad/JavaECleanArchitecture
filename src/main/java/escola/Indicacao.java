37eff6902c0fe512e361ecb8fbf572360a4a6a32
Salomao <salomao.machado.75@gmail.com>
2023-07-18 20:46:30 -0300

    <build> modelando classe indicação com parametros e construtor.

--- src/main/java/escola/Indicacao.java
+++ src/main/java/escola/Indicacao.java
@@ -2,4 +2,11 @@ package escola;
 
 public class Indicacao {
 
+	private Aluno indicado;
+	private Aluno indicante;
+	
+	public Indicacao(Aluno indicado, Aluno indicante) {
+		this.indicado = indicado;
+		this.indicante = indicante;
+	}
 }
