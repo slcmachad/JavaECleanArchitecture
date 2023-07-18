6a9140e241564a4a0b6d47f08c9e3eca0380f082
Salomao <salomao.machado.75@gmail.com>
2023-07-18 20:35:01 -0300

    <test> Criando teste para verificar as validações doa classe telefone.

---
+++ src/test/java/escola/TelefoneTest.java
@@ -0,0 +1,20 @@
+package escola;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TelefoneTest {
+
+    @Test
+    void naoDeveriaCriarTelefoneComDDDsInvalidos() {
+        assertThrows(IllegalArgumentException.class,
+            () -> new Telefone(null, "123456789"));
+
+        assertThrows(IllegalArgumentException.class,
+            () -> new Telefone("", "123456789"));
+
+        assertThrows(IllegalArgumentException.class,
+            () -> new Telefone("1", "123456789"));
+    }
+}
