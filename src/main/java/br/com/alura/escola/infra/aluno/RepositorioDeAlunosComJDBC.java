921a96a14e47a4526bccdd321ab45acda90a044c
Salomao <salomao.machado.75@gmail.com>
2023-07-20 22:06:56 -0300

    <build> Criando parte que será responsavel pela infraEstrutura.

---
+++ src/main/java/br/com/alura/escola/infra/aluno/RepositorioDeAlunosComJDBC.java
@@ -0,0 +1,29 @@
+package br.com.alura.escola.infra.aluno;
+
+import java.util.List;
+
+import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.CPF;
+import br.com.alura.escola.dominio.aluno.RepositorioDeAlunos;
+
+public class RepositorioDeAlunosComJDBC implements RepositorioDeAlunos{
+
+	@Override
+	public void matricular(Aluno aluno) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Aluno buscarPorCpf(CPF cpf) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Aluno> listarTodosAlunoMatriculados() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
