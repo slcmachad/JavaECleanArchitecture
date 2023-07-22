bff368ede42daa0d2f474170d8d50f85c6244e5c
Salomao <salomao.machado.75@gmail.com>
2023-07-21 23:36:53 -0300

    <feat> Criando para isolar codigos que farão parte do processo de matricular um aluno.

---
+++ src/main/java/br/com/alura/escola/aplicacao/aluno/matricular/MatricularAluno.java
@@ -0,0 +1,5 @@
+package br.com.alura.escola.aplicacao.aluno.matricular;
+
+public class MatricularAluno {
+
+}
--- src/main/java/br/com/alura/escola/infra/indicacao/EnviarEmailIndicacaoComJavaMail.java
+++ src/main/java/br/com/alura/escola/infra/indicacao/EnviarEmailIndicacaoComJavaMail.java
@@ -1,5 +1,14 @@
 package br.com.alura.escola.infra.indicacao;
 
-public class EnviarEmailIndicacaoComJavaMail {
+import br.com.alura.escola.aplicacao.indicacao.EnviarEmailIndicacao;
+import br.com.alura.escola.dominio.aluno.Aluno;
+
+public class EnviarEmailIndicacaoComJavaMail implements EnviarEmailIndicacao{
+
+	@Override
+	public void enviarPara(Aluno indicado) {
+
+		
+	}
 
 }
