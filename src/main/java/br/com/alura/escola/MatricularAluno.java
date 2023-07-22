6714cef578ed890129dcc4c8ce661e345d56ffd7
Salomao <salomao.machado.75@gmail.com>
2023-07-21 23:28:46 -0300

    <feat> Instanciando um novo aluno e salvando em memoria.

--- src/main/java/br/com/alura/escola/MatricularAluno.java
+++ src/main/java/br/com/alura/escola/MatricularAluno.java
@@ -1,10 +1,21 @@
 package br.com.alura.escola;
 
+import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.CPF;
+import br.com.alura.escola.dominio.aluno.Email;
+import br.com.alura.escola.dominio.aluno.RepositorioDeAlunos;
+import br.com.alura.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+
 public class MatricularAluno {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String nome = "fulano";
+		CPF cpf = new CPF("123.456.789-00");
+		Email email = new Email("abcd@email.com.br");
+		
+		Aluno aluno = new Aluno(cpf, nome, email);
+		RepositorioDeAlunos repositorio = new RepositorioDeAlunosEmMemoria();
+		repositorio.matricular(aluno);
 	}
 
 }
