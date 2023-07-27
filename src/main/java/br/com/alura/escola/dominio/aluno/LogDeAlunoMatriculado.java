36e8286fb50c663288efdb877611b35c556cabab
Salomao <salomao.machado.75@gmail.com>
2023-07-26 23:37:23 -0300

    <criando metodo que realizará a execução do ouvinte>

--- src/main/java/br/com/alura/escola/dominio/aluno/LogDeAlunoMatriculado.java
+++ src/main/java/br/com/alura/escola/dominio/aluno/LogDeAlunoMatriculado.java
@@ -1,5 +1,14 @@
 package br.com.alura.escola.dominio.aluno;
 
+import java.time.format.DateTimeFormatter;
+
 public class LogDeAlunoMatriculado {
 
+	public void reageAo(AlunoMatriculado evento) {
+		String momentoFormatado = evento.momento().format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
+		System.out.println(String.format(
+				"Aluno com CPF %s matriculado em: %s", 
+				evento.getCfpDoAluno(),
+				momentoFormatado));
+	}
 }
