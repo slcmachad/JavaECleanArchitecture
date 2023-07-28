d3073f581f5c852697d6df8c1f35211427fb96ae
Salomao <salomao.machado.75@gmail.com>
2023-07-27 23:24:04 -0300

    <adicionando parametros a classe matricular para desacoplar o codigo e separar obrigações>

--- src/main/java/br/com/alura/escola/aplicacao/aluno/matricular/MatricularAluno.java
+++ src/main/java/br/com/alura/escola/aplicacao/aluno/matricular/MatricularAluno.java
@@ -1,14 +1,17 @@
 package br.com.alura.escola.aplicacao.aluno.matricular;
 
+import br.com.alura.escola.dominio.PublicadorDeEventos;
 import br.com.alura.escola.dominio.aluno.Aluno;
 import br.com.alura.escola.dominio.aluno.RepositorioDeAlunos;
 
 public class MatricularAluno {
 	
 	private  final RepositorioDeAlunos repositorio;
+	private final PublicadorDeEventos publicador;
 	
-	public MatricularAluno(RepositorioDeAlunos repositorio) {
+	public MatricularAluno(RepositorioDeAlunos repositorio, PublicadorDeEventos publicador) {
 		this.repositorio = repositorio;
+		this.publicador = publicador;
 	}
 	
 	// COMMAND
