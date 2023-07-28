1da82b78e0b5adf0a10b39c55123a4876fd2cab1
Salomao <salomao.machado.75@gmail.com>
2023-07-27 23:43:44 -0300

    <Adicionando atributos e construtores>

--- src/main/java/br/com/alura/escola/dominio/selo/Selo.java
+++ src/main/java/br/com/alura/escola/dominio/selo/Selo.java
@@ -1,5 +1,22 @@
 package br.com.alura.escola.dominio.selo;
 
+import br.com.alura.escola.dominio.aluno.CPF;
+
 public class Selo {
 
+	private CPF cpfDoAluno;
+	private String nome;
+	
+	public Selo(CPF cpf, String nome) {
+		this.cpfDoAluno = cpf;
+		this.nome = nome;
+	}
+
+	public CPF getCpfDoAluno() {
+		return cpfDoAluno;
+	}
+
+	public String getNome() {
+		return nome;
+	}
 }
